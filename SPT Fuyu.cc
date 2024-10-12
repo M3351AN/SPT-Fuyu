de84537dbefb4249ae112001eddf7f4a3fcb8458
渟雲 <zuiun1337@gmail.com>
2024-10-12 19:36:43 +0800

    Add check before execute

--- SPT Fuyu.cc
+++ SPT Fuyu.cc
@@ -36,6 +36,7 @@ void CreateRegistryKeyAndFiles(const std::string& installLocation) {
     std::ofstream(installLocation + "\\UnityCrashHandler64.exe").close();
     std::ofstream(installLocation + "\\WinPixEventRuntime.dll").close();
 }
+
 bool Validate() {//copy from SPT Source
     std::wstring registryPath = L"Software\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\EscapeFromTarkov";
     int v0 = 0;
@@ -73,27 +74,65 @@ bool Validate() {//copy from SPT Source
 
     return v0 == 0;
 }
+
+void Exit() {
+	system("pause");
+    exit(0);
+}
+
 int main(int argc, char* argv[]) {
     SetConsoleTitle(L"SPT Fuyu");
 
     // 判断是否为中文语言 Determine if system language is Chinese
     bool isChinese = IsChineseLanguage();
+	if (Validate()){//检查是否已经通过验证 Check if already passed SPT validation
+        std::cout << (isChinese ? "检测到正版验证已通过，是否重新执行验证？\n(如果您已经安装在线版逃离塔科夫，请不要运行本程序) (y/n): " : "Verification already passed. Do you want to execute verification bypass? \n (If you already installed EFT live version, do not execute this.) (y/n): ");
+        char response;
+        std::cin >> response;
+        if (response != 'y' && response != 'Y') {
+            Exit();
+            return 0;
+        }
+    }
+    std::string filePath;
 
     if (argc != 2) {
-        std::cerr << (isChinese ? "请拖入验证文件存放文件夹。" : "Please drag and drop the folder that you want verification files storage at." )<< std::endl;
+        std::cout << (isChinese ? "未传入文件夹路径。是否要在exe所在目录存放验证文件？(y/n): " : "No folder path dragged in. Do you want to store the verification files in current directory? (y/n): ");
+        char response;
+        std::cin >> response;
+        if (response == 'y' || response == 'Y') {
+            // 获取当前可执行文件的路径 Get the path of the current executable
+            wchar_t exePath[MAX_PATH];
+            GetModuleFileNameW(NULL, exePath, MAX_PATH);
+            filePath = std::filesystem::path(exePath).parent_path().string();
+        }
+        else {
+			Exit();
+			return 0;
+        }
+    }
+    else {
+        filePath = argv[1];
+    }
+
+    // 创建名为SPT Fuyu的文件夹 Create a folder named SPT Fuyu
+    std::string newFolderPath = filePath + "\\SPT Fuyu";
+    if (!std::filesystem::create_directory(newFolderPath)) {
+        std::cerr << (isChinese ? "错误：无法创建SPT Fuyu文件夹。" : "Error: Unable to create SPT Fuyu folder.") << std::endl;
         system("pause");
         return 1;
     }
-
-    std::string filePath = argv[1];
+    filePath = newFolderPath;
 
     if (!std::filesystem::is_directory(filePath)) {
-        std::cerr << (isChinese ? "错误：拖入的不是文件夹。" : "Error: The item dragged in is not a folder.") << std::endl;
+        std::cerr << (isChinese ? "错误：SPT Fuyu路径指向的不是文件夹。" : "Error: The path of SPT Fuyu is not a folder.") << std::endl;
 		system("pause");
         return 1;
     }
+
+
     if (std::filesystem::directory_iterator(filePath) != std::filesystem::end(std::filesystem::directory_iterator())) {
-        std::cerr << (isChinese ? "错误：拖入的文件夹不是空的。" : "Error: The folder dragged in is not empty.") << std::endl;
+        std::cerr << (isChinese ? "错误：SPT Fuyu文件夹不是空的。" : "Error: SPT Fuyu folder is not empty.") << std::endl;
         system("pause");
         return 1;
     }
@@ -104,6 +143,6 @@ int main(int argc, char* argv[]) {
         std::cout << (isChinese ? "验证执行成功." : "Validation Bypass Succeeded.") << std::endl;
     else
         std::cout << (isChinese ? "验证执行失败，未知错误." : "Validation Bypass Failed. Unknown Error") << std::endl;
-    system("pause");
+    Exit();
     return 0;
 }
